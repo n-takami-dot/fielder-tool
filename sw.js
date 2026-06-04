f6d063c87d98f9c79e11bb630a4cc3db7e08d25c
n-takami-dot <n-takami@hyogo-yakult-st.com>
2026-06-04 15:01:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ★ HTMLを更新するたびにこのバージョン番号を上げてください
-const CACHE_VERSION = 'v13';
+const CACHE_VERSION = 'v14';
 const CACHE_NAME = 'fielder-tool-' + CACHE_VERSION;
 
 self.addEventListener('install', event => {
